75213190d923106ea1e08f8267f4382819d4f4d3
Ernesto Linares <ernestol92@hotmail.com>
2025-10-25 20:58:53 -0300

    minor fixes

---
+++ wwwroot/js/hero.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+
+    const heroWrapper = document.querySelector('.hero-wrapper');
+    if (!heroWrapper) return;
+
+    const carousel = heroWrapper.querySelector('.hero-carousel');
+    const backgrounds = carousel.querySelectorAll('.hero-bg');
+    let index = 0;
+
+    const showBg = (i) => {
+        index = (i + backgrounds.length) % backgrounds.length;
+        carousel.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    setInterval(() => {
+        showBg(index + 1);
+        console.log(index);
+    }, 4000);
+
+})
