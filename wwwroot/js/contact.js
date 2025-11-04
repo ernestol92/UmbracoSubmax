9ba11c9973e30ec8840835b0e92215d07c5f2267
Ernesto Linares <ernestol92@hotmail.com>
2025-11-04 11:16:07 -0300

    only portuguese

---
+++ wwwroot/js/contact.js
@@ -0,0 +1,17 @@
+﻿addEventListener('DOMContentLoaded', () => {
+
+    const btn = document.querySelector(".btn-whatsapp");
+
+    btn.addEventListener("click", (e)=>{
+        e.preventDefault();
+        const name = document.getElementById("nome").value;
+        const mensagem = document.getElementById("mensagem").value;
+        const interesse = document.getElementById("interesse").value;
+
+        const phoneNumber = "5582996606790";
+        const text = `Olá, meu nome é ${name}. Tenho interesse em ${interesse}. ${mensagem}`;
+        const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(text)}`;
+        window.open(url, "_blank");
+    })
+
+});
